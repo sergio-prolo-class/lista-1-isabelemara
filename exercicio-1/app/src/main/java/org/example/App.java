55e1a99f766bac0e7421f2e23e68626066d0d628
isabelemara <isabele.m@aluno.ifsc.edu.br>
2025-04-02 21:25:41 -0300

    código funcionando, decidi a abordagem do switch case para determinar a opção da forma, seguindo um raciocinio da linguagem C

--- exercicio-1/app/src/main/java/org/example/App.java
+++ exercicio-1/app/src/main/java/org/example/App.java
@@ -1,8 +1,5 @@
 package org.example;
 
-/// falta quadrado
-
-
 public class App {
     public static void main(String[] args) {
         // Verifica se os parâmetros foram passados corretamente
@@ -11,50 +8,77 @@ public class App {
             return;
         }
 
-        String forma = args[0];  // Forma escolhida (triangulo ou losango)
-        int tamanho = Integer.parseInt(args[1]);  // Tamanho desejado (altura ou tamanho)
+        String forma = args[0];  // Forma escolhida (triangulo, losango ou quadrado)
+        int tamanho = Integer.parseInt(args[1]);  // Tamanho desejado
+        //ou seja precisa entrar com dois parametros, forma e tamanho com --args
 
-        // Condicional para desenhar a forma desejada
-        if (forma.equals("triangulo")) {
-            // Desenhando o triângulo do topo para a base
-            System.out.println("Triangulo:");
-            for (int i = 1; i <= tamanho; i++) {
-                for (int j = 1; j <= i; j++) {
-                    System.out.print("*");
+        // Utilizando switch-case, entrando no raciocinio
+        switch (forma) {
+            case "triangulo":
+                desenharTriangulo(tamanho);
+                break;
+            case "losango":
+                desenharLosango(tamanho);
+                break;
+            case "retangulo":
+                if (args.length < 3) {
+                    System.out.println("Por favor, forneça a altura e a largura para o retangulo.");
+                    return;
                 }
-                System.out.println();
-            }
-        } else if (forma.equals("losango")) {
-            // Desenhando o losango
-            System.out.println("Losango:");
+                int largura = Integer.parseInt(args[2]);
+                desenharRetangulo(tamanho, largura);
+                break;
+            default:
+                System.out.println("Forma inválida!");
+        }
+    }
 
-            // Parte superior do losango
-            for (int i = 1; i <= tamanho; i += 2) {
-                // Espaços antes dos asteriscos
-                for (int j = 0; j < (tamanho - i) / 2; j++) {
+    public static void desenharTriangulo(int tamanho) {
+        System.out.println("Triângulo:");
+        for (int i = 1; i <= tamanho; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void desenharLosango(int tamanho) {
+        System.out.println("Losango:");
+
+        //tamanho precisa ser impar, e meio seria o meio do losango
+        int meio = tamanho / 2;
+        for (int i = 0; i <= meio; i++) {
+            for (int j = 0; j < meio - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = meio - 1; i >= 0; i--) {
+            for (int j = 0; j < meio - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void desenharRetangulo(int altura, int largura) {
+        System.out.println("Retangulo:");
+        for (int i = 0; i < altura; i++) {
+            for (int j = 0; j < largura; j++) {
+                if (i == 0 || i == altura - 1 || j == 0 || j == largura - 1) {
+                    System.out.print("*");
+                } else {
                     System.out.print(" ");
                 }
-                // Asteriscos
-                for (int j = 0; j < i; j++) {
-                    System.out.print("*");
-                }
-                System.out.println();
             }
-
-            // Parte inferior do losango
-            for (int i = tamanho - 2; i > 0; i -= 2) {
-                // Espaços antes dos asteriscos
-                for (int j = 0; j < (tamanho - i) / 2; j++) {
-                    System.out.print(" ");
-                }
-                // Asteriscos
-                for (int j = 0; j < i; j++) {
-                    System.out.print("*");
-                }
-                System.out.println();
-            }
-        } else {
-            System.out.println("Forma inválida!");
+            System.out.println();
         }
     }
 }
